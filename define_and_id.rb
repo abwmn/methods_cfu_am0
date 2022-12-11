5035bc8ad859e4d6f05ae626354f30bb9ec554a2
Andrew Bowman <andrewtbowman@gmail.com>
2022-12-10 21:07:27 -0500

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,17 +11,22 @@
 
 
 "Hello World".include?("Hello")
-
+# The include? method is called on the string object "Hello World" 
+# "Hello" is passed as the argument, so the Boolean query returns true
 
 "Hello World".end_with?("Hello")
-
+# The end_with? method is called on the string object "Hello World"
+# "Hello" is passed as the argument, so the Boolean query returns false
 
 "Hello World".end_with?("rld")
-
+# The end_with? method is called on the string object "Hello World"
+# "rld" is passed as the argument, so the Boolean query returns true
 
 12.even?
-
+# The even? method is called on the integer 12
+# The Boolean query checks whether 12 is even and returns true
 
 18.next
-
+# The next method is called on the integer 18
+# The method returns the integer +1, so 19
 
